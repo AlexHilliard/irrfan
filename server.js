b4ed4a23746910bfd89f2c4fd9cadfdd24eaef1c
Alexander Hilliard <lv64giratina@gmail.com>
2020-01-08 15:48:44 -0700

    asdfarf

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-let port = process.env.PORT | 8888
+let port = process.env.PORT || 8888
 
 app.get("/", (req,res)=>{
     res.sendFile(__dirname+"/falloutinfo.html")
