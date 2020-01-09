86bdfa6afe05f1022cca3e0eeb6094b4e49e400b
Alexander Hilliard <lv64giratina@gmail.com>
2020-01-09 07:54:53 -0700

    hhh

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get("/", (req,res)=>{
     res.sendFile(__dirname+"/falloutinfo.html")
 })
 
-app.get("/nuemont", (req,res)=>{
-    res.sendFile(__dirname+"/https://www.neumont.edu/")
+app.get("/home", (req,res)=>{
+    res.sendFile(__dirname+"/index.html ")
 })
 app.listen(port, ()=>console.log(port))
