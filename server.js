47a52e03c90f2de5ed0b388acc630a31584b97a2
Alexander Hilliard <lv64giratina@gmail.com>
2020-01-08 23:50:42 -0700

    awxs

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get("/", (req,res)=>{
 })
 
 app.get("/nuemont", (req,res)=>{
-    res.sendFile(__dirname+"/index.html")
+    res.sendFile(__dirname+"/https://www.neumont.edu/")
 })
 app.listen(port, ()=>console.log(port))
