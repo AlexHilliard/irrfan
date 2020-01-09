fe6d1ac66abd4077e7c4df47919fd5c06de740ea
Alexander Hilliard <lv64giratina@gmail.com>
2020-01-08 23:32:46 -0700

    nuemont

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ let port = process.env.PORT || 8888
 app.get("/", (req,res)=>{
     res.sendFile(__dirname+"/falloutinfo.html")
 })
+
+app.get("/nuemont", (req,res)=>{
+    res.sendFile(__dirname+"/falloutinfo.html")
+})
 app.listen(port, ()=>console.log(port))
