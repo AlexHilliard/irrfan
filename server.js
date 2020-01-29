b2469cc014f41e902689133ac0607690344a8323
Alexander Hilliard <lv64giratina@gmail.com>
2020-01-29 14:33:38 -0700

    LINK

--- server.js
+++ server.js
@@ -8,5 +8,26 @@ app.get("/", (req,res)=>{
 app.get("/PHB", (req,res)=>{
     res.sendFile(__dirname+"/falloutPHB.html")
 })
+app.get("/Races-Vault_Dweller", (req,res)=>{
+    res.sendFile(__dirname+"/falloutRVD.html")
+})
+app.get("/Races-Ghoul", (req,res)=>{
+    res.sendFile(__dirname+"/falloutRG.html")
+})
+app.get("/Races-Super_Mutant", (req,res)=>{
+    res.sendFile(__dirname+"/falloutRSM.html")
+})
+app.get("/Races-Synth", (req,res)=>{
+    res.sendFile(__dirname+"/falloutRS.html")
+})
+app.get("/Races-Protectotron", (req,res)=>{
+    res.sendFile(__dirname+"/falloutRP.html")
+})
+app.get("/Races-Wastelander", (req,res)=>{
+    res.sendFile(__dirname+"/falloutRW.html")
+})
+app.get("/Races-Zeta", (req,res)=>{
+    res.sendFile(__dirname+"/falloutRZ.html")
+})
 app.use(express.static(__dirname));
 app.listen(port, ()=>console.log(port))
