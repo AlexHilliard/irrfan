9180d6f06046105d9bee5bb7322f6d245a6c618b
Alexander Hilliard <lv64giratina@gmail.com>
2020-01-29 12:49:39 -0700

    massive overhaul

--- pagescript.js
+++ pagescript.js
@@ -11,19 +11,7 @@ for (i = 0; i < coll.length; i++) {
       content.style.display = "block";
     }
   });
+}
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-// var coll = document.getElementsByClassName("collapsible");
-// var i;
-
-// for (i = 0; i < coll.length; i++) {
-//   coll[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var content = this.nextElementSibling;
-//     if (content.style.maxHeight){
-//       content.style.maxHeight = null;
-//     } else {
-//       content.style.maxHeight = content.scrollHeight + "px";
-//     }
-//   });
-// }
-// }
