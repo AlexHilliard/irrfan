c93c7a947a78df0c24b36bd0c483c339e4cfdf3d
Alexander Hilliard <lv64giratina@gmail.com>
2020-01-30 09:05:44 -0700

    adsf

--- server.js
+++ server.js
@@ -29,5 +29,54 @@ app.get("/Races-Wastelander", (req,res)=>{
 app.get("/Races-Zeta", (req,res)=>{
     res.sendFile(__dirname+"/falloutRZ.html")
 })
+app.get("/Classes-Brawler", (req,res)=>{
+    res.sendFile(__dirname+"/falloutCB.html")
+})
+app.get("/Classes-Courtier", (req,res)=>{
+    res.sendFile(__dirname+"/falloutCC.html")
+})
+app.get("/Classes-Friend_of_Animals", (req,res)=>{
+    res.sendFile(__dirname+"/falloutCFoA.html")
+})
+app.get("/Classes-Gunslinger", (req,res)=>{
+    res.sendFile(__dirname+"/falloutCG.html")
+})
+app.get("/Classes-Scientist", (req,res)=>{
+    res.sendFile(__dirname+"/falloutCSci.html")
+})
+app.get("/Classes-Survivalist", (req,res)=>{
+    res.sendFile(__dirname+"/falloutCSur.html")
+})
+app.get("/Classes-Raider", (req,res)=>{
+    res.sendFile(__dirname+"/falloutCR.html")
+})
+app.get("/Classes-Wanderer", (req,res)=>{
+    res.sendFile(__dirname+"/falloutCWan.html")
+})
+app.get("/Classes-Wasteland_Medic", (req,res)=>{
+    res.sendFile(__dirname+"/falloutCM.html")
+})
+app.get("/Feats", (req,res)=>{
+    res.sendFile(__dirname+"/falloutF.html")
+})
+app.get("/Skills", (req,res)=>{
+    res.sendFile(__dirname+"/falloutS.html")
+})
+app.get("/Traits", (req,res)=>{
+    res.sendFile(__dirname+"/falloutT.html")
+})
+app.get("/Equipment-Armor", (req,res)=>{
+    res.sendFile(__dirname+"/falloutEA.html")
+})
+app.get("/Equipment-Weapons", (req,res)=>{
+    res.sendFile(__dirname+"/falloutEW.html")
+})
+app.get("/Items", (req,res)=>{
+    res.sendFile(__dirname+"/falloutI.html")
+})
+app.get("/GMing", (req,res)=>{
+    res.sendFile(__dirname+"/falloutGM.html")
+})
+
 app.use(express.static(__dirname));
 app.listen(port, ()=>console.log(port))
